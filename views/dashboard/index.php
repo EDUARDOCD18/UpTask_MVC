03bf30481e55d2bd311415d652ec6ab4c334eefd
eduardocd18 <javierecd@gmail.com>
2025-06-11 19:43:38 -0400

    598. Finalizando el CSS del Dashboard

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -5,5 +5,8 @@
 
         <?php include_once __DIR__ . '/../templates/barra.php' ?>
 
+        <div class="contenido">
+            <h2 class="nombre-pagina"><?php echo $titulo ?></h2>
+        </div>
     </div>
 </div>

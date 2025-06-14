bc4fa7fcecf0b0512809166a05a80cff073bc6ab
eduardocd18 <javierecd@gmail.com>
2025-06-14 11:13:28 -0400

    602. Creando el Modelo de Proyectos

---
+++ models/Proyecto.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Proyecto;
+
+use Model\ActiveRecord;
+
+/* MODELO PARA PROYECTOS */
+
+#[\AllowDynamicProperties]
+class Proyecto extends ActiveRecord
+{
+    protected static $tabla = 'proyectos';
+    protected static $columnasDB = ['id', 'proyecto', 'url', 'propietarioId'];
+
+    public $id;
+    public $proyecto;
+    public $url;
+    public $propietarioId;
+
+    /* CONSTRUCTOR */
+    public function __construct($args = [])
+    {
+        $this->id = $args['id'] ?? null;
+        $this->proyecto = $args['proyecto'] ?? '';
+        $this->url = $args['url'] ?? '';
+        $this->propietarioId = $args['propietarioId'] ?? '';
+    }
+}

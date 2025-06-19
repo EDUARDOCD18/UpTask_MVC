2516b262516927307b93788be6244eef26c07b23
eduardocd18 <javierecd@gmail.com>
2025-06-19 08:23:23 -0400

    608. Aplicando CSS a los Proyectos

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -3,8 +3,7 @@
 <?php include_once __DIR__ . '/header-dashboard.php'; ?>
 
 <?php if (count($proyectos) === 0) { ?>
-    <p class="no-proyectos">No hay proyectos aún</p>
-    <a href="/crear-proyecto">Crea un proyecto</a>
+    <p class="no-proyectos">No hay proyectos aún <a href="/crear-proyecto">Crea un proyecto</a></p>
 <?php } else { ?>
     <ul class="listado-proyectos">
         <?php foreach ($proyectos as $proyecto) { ?>

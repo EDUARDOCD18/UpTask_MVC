50878505cdd036d8e9b6ec4dfcc3342cfeb97bee
eduardocd18 <javierecd@gmail.com>
2025-06-21 20:58:21 -0400

    611. Creando el Modal para añadir Tareas

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -6,6 +6,25 @@
   });
 
   function mostrarFormulario() {
-    alert("Hola")
+    const modal = document.createElement("DIV"); // Crea el modal
+    modal.classList.add("modal"); // Agrega la clase modal
+
+    /* Crea el formulario HTML en el modal */
+    modal.innerHTML = `
+    <form class="formulario nueva-tarea">
+      <legend>Añade una nueva tarea</legend>
+      <div class="campo">
+        <label for="">Tarea</label>
+        <input type="text" name="tarea" placeholder="Añadir tarea" id="tarea" />
+      </div>
+
+      <div class="opciones">
+        <input type="submit" class="submit-nueva-tarea" value="Añadir" />
+        <button type="button" class="cerrar-modal">Cancelar</button>
+      </div>
+    </form>
+    `;
+
+    document.querySelector("body").appendChild(modal); // Agregal el modal al documento
   }
 })();

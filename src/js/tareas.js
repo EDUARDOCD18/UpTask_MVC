6f67bdefdc3b0dcd7e90458baecf4b4b9be0f6fc
eduardocd18 <javierecd@gmail.com>
2025-06-21 20:42:57 -0400

    610. Añadiendo JavaScript para mostrar el Formulario

---
+++ src/js/tareas.js
@@ -0,0 +1,11 @@
+(function () {
+  // Botón para mostrar el Modal de Agregar tarea
+  const nuevaTareaBtn = document.querySelector("#agregar-tarea");
+  nuevaTareaBtn.addEventListener("click", function () {
+    mostrarFormulario();
+  });
+
+  function mostrarFormulario() {
+    alert("Hola")
+  }
+})();

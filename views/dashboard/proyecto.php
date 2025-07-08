2258482d5d2a5793463d26bd508d483802e7ff88
eduardocd18 <javierecd@gmail.com>
2025-07-08 04:55:07 -0400

    643. Creando el Formularo

--- views/dashboard/proyecto.php
+++ views/dashboard/proyecto.php
@@ -7,6 +7,31 @@
         <button type="button" class="agregar-tarea" id="agregar-tarea">+ Nueva tarea</button>
     </div>
 
+    <!-- Filtros para las tares -->
+    <div class="filtros" id="filtros">
+        <div class="filtros-inputs">
+            <h2>Filtros:</h2>
+
+            <!-- Filtrar por todas las tarea -->
+            <div class="campo">
+                <label for="todas">Todas</label>
+                <input type="radio" id="todas" name="filtro" value="" checked>
+            </div>
+
+            <!-- Filtrar por completadas -->
+            <div class="campo">
+                <label for="todas">Completadas</label>
+                <input type="radio" id="Completadas" name="filtro" value="1">
+            </div>
+
+            <!-- Filtrar por pendientes -->
+            <div class="campo">
+                <label for="todas">Pendientes</label>
+                <input type="radio" id="Pendientes" name="filtro" value="0">
+            </div>
+        </div>
+    </div>
+
     <ul id="listado-tareas" class="listado-tareas">
 
     </ul>

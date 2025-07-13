c0ea87231690d13f4a388c28af647454a1e354a5
eduardocd18 <javierecd@gmail.com>
2025-07-13 04:52:54 -0400

    650. Mostrando / Ocultando el Menú

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,22 @@
 const mobileMenuBtn = document.querySelector("#mobile-menu");
+const cerrarMenuBtn = document.querySelector("#cerrar-menu");
 const sidebar = document.querySelector(".sidebar");
 
+/* Mostrar el menú mobile */
 if (mobileMenuBtn) {
   mobileMenuBtn.addEventListener("click", function () {
-    sidebar.classList.toggle("mostrar");
+    sidebar.classList.add("mostrar"); // Añande la clase que que muestra el menú mobile
+  });
+}
+
+/* Remover el menú mobile */
+if (cerrarMenuBtn) {
+  cerrarMenuBtn.addEventListener("click", function () {
+    sidebar.classList.add("ocultar");
+
+    setTimeout(() => {
+      sidebar.classList.remove("mostrar"); // Remueve la clase que que muestra el menú mobile
+      sidebar.classList.remove("ocultar");
+    }, 800);
   });
 }
